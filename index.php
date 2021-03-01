5ba842641e8bd857ea8fe14d4419d85bc9ab04ef
Beny Ibrani <beny.ibrani@vivagroup.co.id>
2021-03-01 20:35:12 +0800

    update

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-echo "Hi There, Im CI / CD Demo in AlibabaCloud Kubernetes <br \>";
+echo "Hi There, Im CI / CD Demo in Huaweicloud <br \>";
 echo "==================================================== <br \><br \>";
-echo "AlibabaCloud Kubernetes <br \>";
+echo "HuaweiCloud <br \>";
 echo "You're accessing from IP Address: ".$_SERVER['REMOTE_ADDR'].'<br \>';
 echo "========================================================== <br \><br \>";
 echo "The time in Jakarta is " . date("h:i:sa"). " PindahKantorBaru ";
